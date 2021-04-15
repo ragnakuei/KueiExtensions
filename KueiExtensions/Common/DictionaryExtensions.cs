f0579a7e1773115c06c30d66b2a3319b94ce63f7
Kuei <ragnakuei@livemail.tw>
2021-04-15 12:26:58 +0800

    add GetValueOrNull

---
+++ KueiExtensions/Common/DictionaryExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace KueiExtensions.Common
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue? GetValueOrNull<TKey, TValue>(this Dictionary<TKey?, TValue?> dict, TKey? key)
+            where TKey : struct
+            where TValue : struct
+        {
+            if (dict                        == null
+             || key.GetValueDefaultOrNull() == null)
+            {
+                return null;
+            }
+
+            return dict?.GetValueOrDefault(key);
+        }
+
+        public static TValue? GetValueOrNull<TKey, TValue>(this Dictionary<TKey, TValue?> dict, TKey key)
+            where TKey : class
+            where TValue : struct
+        {
+            if (dict == null
+             || key  == null)
+            {
+                return null;
+            }
+
+            return dict?.GetValueOrDefault(key);
+        }
+
+        public static TValue GetValueOrNull<TKey, TValue>(this Dictionary<TKey?, TValue> dict, TKey? key)
+            where TKey : struct
+            where TValue : class
+        {
+            if (dict                        == null
+             || key.GetValueDefaultOrNull() == null)
+            {
+                return null;
+            }
+
+            return dict?.GetValueOrDefault(key);
+        }
+
+        public static TValue GetValueOrNull<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
+            where TKey : class
+            where TValue : class
+        {
+            if (dict == null
+             || key  == null)
+            {
+                return null;
+            }
+
+            return dict?.GetValueOrDefault(key);
+        }
+    }
+}
