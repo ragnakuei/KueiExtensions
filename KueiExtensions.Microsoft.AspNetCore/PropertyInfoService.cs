e5101ca20eb70277eeffb4681a7996b27d5f03b2
Kuei <ragnakuei@livemail.tw>
2022-02-24 16:39:25 +0800

    KueiExtensions.Microsoft.AspNetCore - add PropertyInfoService

---
+++ KueiExtensions.Microsoft.AspNetCore/PropertyInfoService.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using KueiExtensions.Microsoft.AspNetCore.Models;
+
+namespace KueiExtensions.Microsoft.AspNetCore;
+
+public class PropertyInfoService
+{
+    public PropertyInfoService()
+    {
+    }
+
+    public Dictionary<string, PropertyInfoDto> GetProperties(Type type)
+    {
+        return GetPropertiesWithAction(type, dto => true);
+    }
+
+    public Dictionary<string, PropertyInfoDto> GetDisplayNameProperties(Type type)
+    {
+        return GetPropertiesWithAction(type, dto => dto.DisplayAttribute != null);
+    }
+
+    private Dictionary<string, PropertyInfoDto> GetPropertiesWithAction(Type type, Func<PropertyInfoDto, bool> whereAction)
+    {
+        var result = type.GetProperties()
+                         .Select(p =>
+                                 {
+                                     var dto = new PropertyInfoDto();
+                                     dto.PropertyName         = p.Name;
+                                     dto.DisplayAttribute     = p.GetCustomAttributes(typeof(DisplayAttribute), false)?.FirstOrDefault() as DisplayAttribute;
+                                     dto.ValidationAttributes = GetValidationAttributes(p);
+                                     dto.PropertyInfo         = p;
+                                     dto.ElementType = new[]
+                                                       {
+                                                           p.PropertyType.GetElementType(),
+                                                           p.PropertyType.GetGenericArguments().FirstOrDefault(gt => gt.IsValueType == false)
+                                                       }.FirstOrDefault(t => t != null);
+
+                                     dto.IsClassObject = dto.IsCollection                          == false
+                                                      && dto.PropertyInfo.PropertyType.IsValueType == false
+                                                      && dto.PropertyInfo.PropertyType             != typeof(string);
+
+                                     if (dto.IsClassObject)
+                                     {
+                                         dto.ObjectType = dto.PropertyInfo.PropertyType;
+                                     }
+
+                                     return dto;
+                                 })
+                         .Where(whereAction)
+                         .ToDictionary(k => k.PropertyName,
+                                       v => v);
+
+        return result;
+    }
+
+    private Dictionary<string, object> GetValidationAttributes(PropertyInfo propertyInfo)
+    {
+        var result = new Dictionary<string, object>();
+
+        var customAttributes = propertyInfo.GetCustomAttributes().ToArray();
+
+        AssignRequiredValidationRule(result, customAttributes);
+        AssignStringLengthValidationRule(result, customAttributes);
+        AssignNumberRangeValidationRule(result, customAttributes);
+
+        return result;
+    }
+
+    private void AssignRequiredValidationRule(Dictionary<string, object> validationRules,
+                                              Attribute[]                customAttributes)
+    {
+        var requiredAttribute = customAttributes.OfType<RequiredAttribute>().FirstOrDefault();
+        if (requiredAttribute == null)
+        {
+            return;
+        }
+
+        validationRules.Add("required", true);
+    }
+
+    private void AssignStringLengthValidationRule(Dictionary<string, object> validationRules,
+                                                  Attribute[]                customAttributes)
+    {
+        var requiredAttribute = customAttributes.OfType<StringLengthAttribute>().FirstOrDefault();
+        if (requiredAttribute == null)
+        {
+            return;
+        }
+
+        if (requiredAttribute.MaximumLength > 0)
+        {
+            validationRules.Add("maxLength", requiredAttribute.MaximumLength);
+        }
+
+        if (requiredAttribute.MinimumLength > 0)
+        {
+            validationRules.Add("minLength", requiredAttribute.MinimumLength);
+        }
+    }
+
+    private void AssignNumberRangeValidationRule(Dictionary<string, object> validationRules,
+                                                 Attribute[]                customAttributes)
+    {
+        var rangeAttribute = customAttributes.OfType<RangeAttribute>().FirstOrDefault();
+        if (rangeAttribute == null)
+        {
+            return;
+        }
+
+        validationRules.Add("min", rangeAttribute.Minimum);
+        validationRules.Add("max", rangeAttribute.Maximum);
+    }
+}
---
+++ KueiExtensionsTests/Microsoft.AspNetCore/PropertyInfoServiceTest/GetValidationRuleTests.cs
@@ -0,0 +1,121 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using FluentAssertions;
+using KueiExtensions.Microsoft.AspNetCore;
+using KueiExtensions.Microsoft.AspNetCore.Models;
+using NUnit.Framework;
+
+namespace KueiExtensionsTests.Microsoft.AspNetCore.PropertyInfoServiceTest;
+
+public class GetValidationRuleTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    private class TestRequiredDto
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+
+    [Test]
+    public void TestRequiredDto01()
+    {
+        var target = new PropertyInfoService();
+
+        var actual = GetValidationRulesResult(target.GetProperties(typeof(TestRequiredDto)));
+
+        var expected = new Dictionary<string, Dictionary<string, object>>
+                       {
+                           ["Name"] = new()
+                                      {
+                                          ["required"] = true,
+                                      }
+                       };
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    private class TestStringMaxLengthDto
+    {
+        [StringLength(10)]
+        public string Name { get; set; }
+    }
+
+    [Test]
+    public void TestStringMaxLength()
+    {
+        var target = new PropertyInfoService();
+
+        var actual = GetValidationRulesResult(target.GetProperties(typeof(TestStringMaxLengthDto)));
+
+        var expected = new Dictionary<string, Dictionary<string, object>>
+                       {
+                           ["Name"] = new()
+                                      {
+                                          ["maxLength"] = 10,
+                                      }
+                       };
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    private class TestStringMaxMinLengthDto
+    {
+        [StringLength(10, MinimumLength = 3)]
+        public string Name { get; set; }
+    }
+
+    [Test]
+    public void TestStringMaxMinLength()
+    {
+        var target = new PropertyInfoService();
+
+        var actual = GetValidationRulesResult(target.GetProperties(typeof(TestStringMaxMinLengthDto)));
+
+        var expected = new Dictionary<string, Dictionary<string, object>>
+                       {
+                           ["Name"] = new()
+                                      {
+                                          ["maxLength"] = 10,
+                                          ["minLength"] = 3,
+                                      }
+                       };
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    private class TestNumberMinMaxLengthDto
+    {
+        [global::System.ComponentModel.DataAnnotations.Range(10, 20)]
+        public string Name { get; set; }
+    }
+
+    [Test]
+    public void TestNumberMinMaxLength()
+    {
+        var target = new PropertyInfoService();
+
+        var actual = GetValidationRulesResult(target.GetProperties(typeof(TestNumberMinMaxLengthDto)));
+
+        var expected = new Dictionary<string, Dictionary<string, object>>
+                       {
+                           ["Name"] = new()
+                                      {
+                                          ["min"] = 10,
+                                          ["max"] = 20,
+                                      }
+                       };
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    private Dictionary<string, Dictionary<string, object>> GetValidationRulesResult(Dictionary<string, PropertyInfoDto> properties)
+    {
+        var result = properties.ToDictionary(kv => kv.Key, kv => kv.Value.ValidationAttributes);
+        return result;
+    }
+}
