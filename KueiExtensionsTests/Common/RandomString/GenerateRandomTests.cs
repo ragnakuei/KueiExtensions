386ef471a434526328d3f35ac1b05d30855a65a9
Kuei <ragnakuei@livemail.tw>
2022-03-02 14:53:22 +0800

    KueiExtensions - add RandomString

---
+++ KueiExtensionsTests/Common/RandomString/GenerateRandomTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace KueiExtensionsTests.Common.RandomString;
+
+public class GeneratTests
+{
+    private readonly int _length = 100;
+    private readonly int _count  = 100;
+
+    [Test]
+    public void LowerCaseOnly()
+    {
+        var target = new KueiExtensions.RandomString(useLowerCase: true);
+
+        for (int i = 0; i < _count; i++)
+        {
+            var actual      = target.Generate(_length);
+            var lowerActual = actual.ToLower();
+            Assert.AreEqual(lowerActual, actual);
+        }
+    }
+
+    [Test]
+    public void UpperCaseOnly()
+    {
+        var target = new KueiExtensions.RandomString(userUpperCase: true);
+
+        for (int i = 0; i < _count; i++)
+        {
+            var actual      = target.Generate(_length);
+            var lowerActual = actual.ToUpper();
+            Assert.AreEqual(lowerActual, actual);
+        }
+    }
+
+    [Test]
+    public void AddiditionalChars()
+    {
+        var target = new KueiExtensions.RandomString(additionalChars: new char[] { 'a' });
+
+        var actual         = target.Generate(_length);
+        var distinctActual = actual.Distinct().ToArray();
+
+        var expected = new char[] { 'a' };
+
+        expected.Should().BeEquivalentTo(distinctActual);
+    }
+}
